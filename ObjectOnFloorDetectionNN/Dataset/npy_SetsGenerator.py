0973a3bc34169e96fec4b5a68b62ada6699f7c68
Harry Miranda <Harry Miranda Santiago>
2016-07-25 09:40:24 -0400

    Bugs Fixed on the npy generator

--- ObjectOnFloorDetectionNN/Dataset/npy_SetsGenerator.py
+++ ObjectOnFloorDetectionNN/Dataset/npy_SetsGenerator.py
@@ -13,31 +13,20 @@ savingDirectory = "/home/harry/Documents/Gits/YuengGit/ObjectOnFloorDetectionNN/
 images, labels = sorted(os.listdir(directory + "/Images")), sorted(os.listdir(directory + "/Labels"))
 
 # Empty lists that will contain the npy array equivalents of the pictures
-npyTrainingImages, npyTrainingLabels = [], []
-npyTestingImages, npyTestingLabels = [], []
+npyTrainingImages= []
+npyTestingImages = []
 
 if len(images) == len(labels): # Validation of the size of the images and labels
   # for loop that manage the list of images, labels and iteration number
   for i, l, z in zip(images, labels, range(len(images))):
     if ".jpeg" or ".JPEG" in i and l: # Validation of the extensions of the images and labels
-      if z < 12000: # If that append the images and labels from iteration 0 - 9999 to the Training Lists
+      if z < 11999: # If that append the images and labels from iteration 0 - 9999 to the Training Lists
         # print the name of the images and labels for check the correlation of them
         print("Adding to training files: " + str(i) + " num: " + str(z) + "\n" + "Adding to training files: " + str(l) + " num: " + str(z) + "\n\n")
 
         # Appending the images to the training lists
         npyTrainingImages.append(cv2.imread(directory + 'Images/' + str(i)))
       else: #else that will append the images and labels from iteration 1000 - (len(images) - 1) to the Testing Lists
-        if z == 10000: # If that will save the training npy array into npy files
-          # Printing the status of the code: Status of the npy array creation
-          print("\n\tCreating the npy arrays of the Training sets...\n")
-
-          print("\n\tShape of npyTrainingImages: " + str(npyTrainingImages.shape))
-          # saving the arrays of the Training sets as .npy to the file system
-          print("\n\tSaving the npy files of the Training sets...\n")
-          print (savingDirectory)
-          np.save(savingDirectory + 'npyTrainingImages', npyTrainingImages)
-          
-        # print the name of the images and labels for check the correlation of them
         print("Adding to testing files: " + str(i) + " num: " + str(z) + "\n" + "Adding to testing files: " + str(l) + " num: " + str(z) + "\n\n")
 
         npyTestingImages.append(cv2.imread(directory + 'Images/' + str(i)))
@@ -48,10 +37,14 @@ if len(images) == len(labels): # Validation of the size of the images and labels
   print("npyTestingImages npy array created!")
   
   print("\n\tShape of npyTestingImages: " + str(npyTestingImages.shape))
+  print("\n\tShape of npyTrainingImages: " + str(npyTrainingImages.shape))
   
   # saving the arrays of the Training sets as .npy to the file system
   print("\n\tSaving the npy files of the Testing sets...\n")
   np.save(savingDirectory + 'npyTestingImages', npyTestingImages)
+  print("\n\tSaving the npy files of the Training sets...\n")
+  print (savingDirectory)
+  np.save(savingDirectory + 'npyTrainingImages', npyTrainingImages)
   
 else:
   # message printed if the length of the images and length are not the same
